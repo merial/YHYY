fdfa092b3aa426d5dd0b6c08cb32f3d757eba2aa
merial <meri.alho@gmail.com>
2015-09-21 22:14:02 +0300

    lisää shittii

--- SIMULAATTORI/src/simulaattori/MerinKontrolleri.java
+++ SIMULAATTORI/src/simulaattori/MerinKontrolleri.java
@@ -1,21 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package simulaattori;
 
-/**
- *
- * @author Omistaja
- */
 public class MerinKontrolleri {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    private Ihminen hp;
+    private Jääkaappi ruoka;
+    private Rng numero;
+    
+    
+        
+        
+        
+        
+        
+    
     
 }
