36f75acdafc2a6a065559f2caf80db7721b822aa
merial <meri.alho@gmail.com>
2015-09-25 09:17:12 +0300

    lisää shittii

--- SIMULAATTORI/src/simulaattori/MerinKontrolleri.java
+++ SIMULAATTORI/src/simulaattori/MerinKontrolleri.java
@@ -8,6 +8,9 @@ public class MerinKontrolleri {
     private Ihminen hp;
     private Jääkaappi ruoka;
     private Rng numero;
+    private Tulipalo palo;
+    private Keittiö syö;
+    
     
     
         
