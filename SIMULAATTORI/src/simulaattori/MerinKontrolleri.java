351a917a6946f7b67e8d7913cfda8166b61b5c90
merial <meri.alho@gmail.com>
2015-09-25 11:31:31 +0300

    lisää shittii

--- SIMULAATTORI/src/simulaattori/MerinKontrolleri.java
+++ SIMULAATTORI/src/simulaattori/MerinKontrolleri.java
@@ -12,15 +12,38 @@ public class MerinKontrolleri {
     private Keittiö syö;
     private Syöpä tauti;
     
+    MerinKontrolleri(){
+    }
+    
+    MerinKontrolleri(Ihminen hp){
+        this.hp = hp;
+    }
+    
+    MerinKontrolleri(Jääkaappi ruoka){
+        this.ruoka=ruoka;
+    }
+    
+    MerinKontrolleri(R_N_G numero){
+        this.numero=numero;
+    }
+    
+    MerinKontrolleri(Tulipalo palo){
+        this.palo=palo;
+    }
+    
+    MerinKontrolleri(Keittiö syö){
+        this.syö=syö;
+    }
+    
+    MerinKontrolleri(Syöpä tauti){
+        this.tauti= tauti;
+    }
+    
+    public void kuoletko(){
+        palo.kuoletko();
+    }
     
     
-    
-    
-        
-        
-        
-        
-        
-    
-    
+ 
+  
 }
