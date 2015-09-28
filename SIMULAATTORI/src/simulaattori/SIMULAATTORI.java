c3b216d629082ae3c79d9ba07cab40dd6498ff2f
tinos <tinos@V-315-02.edu.wks.metropolia.fi>
2015-09-28 14:50:56 +0300

    Merge origin/master
    Conflicts:
    	SIMULAATTORI/src/simulaattori/Hukkuminen.java
    	SIMULAATTORI/src/simulaattori/WC.java

--- SIMULAATTORI/src/simulaattori/SIMULAATTORI.java
+++ SIMULAATTORI/src/simulaattori/SIMULAATTORI.java
@@ -1,31 +1,25 @@
-
 package simulaattori;
 
 import javax.swing.*;
 
 public class SIMULAATTORI {
-    
+
     public static void main(String[] args) {
-        
+
         Kontrolleri omaKontrolleri = new Kontrolleri();
         char K, k, E, e;
         int valinta;
         String nimi;
-        
-        
-        
+
         JOptionPane.showMessageDialog(null, "Tervetuloa SIMulaatioon!\n"
                 + "Voit liikkua huoneissa ja tehdä jotain ja jotain öö");
-        
-        
+
         nimi = JOptionPane.showInputDialog(null, "Kerro nimesi");
-        
-        
-        JOptionPane.showMessageDialog(null, "Nimesi on siis "+omaKontrolleri.getNimi());
-        
+
+        JOptionPane.showMessageDialog(null, "Nimesi on siis " + omaKontrolleri.getNimi());
+
         System.exit(0);
-        
-        
+
     }
-    
+
 }
