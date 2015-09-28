12e0fba062a543ebbaa39fe07faa2e34d4c6a25c
merial <meri.alho@gmail.com>
2015-09-28 13:49:16 +0300

    lisää shittii

--- SIMULAATTORI/src/simulaattori/Kontrolleri.java
+++ SIMULAATTORI/src/simulaattori/Kontrolleri.java
@@ -14,8 +14,11 @@ public class Kontrolleri {
     private Uimaallas allas;
     private WC vessa;
     private Hukkuminen huku;
-    private Kylpyamme kylve;
     private Käytävä kaytava;
+    public String nimi;
+    public int hela;
+    public int miinushela;
+    public int plushela;
     
     
     Kontrolleri(){
@@ -34,9 +37,31 @@ public class Kontrolleri {
         
     }
     
-
-    public void kuoletko(){
-        palo.kuoletko();
+    public String setNimi(){
+        hp.setNimi(nimi);
+        return null;
     }
+    public String getNimi(){
+        return hp.getNimi();
+    }
+    public void setHp(){
+        hp.setHp(hela);
+    }
+    public int getHp(){
+        return hp.getHp();
+    }
+    public void vahennaHp(){
+        hp.vähennä(miinushela);
+    }
+    public void lisaaHp(){
+        hp.lisää(plushela);
+    }
+    
+    
+    
+    public String kuoletko(){
+        return palo.kuoletko();
+    }
+    
     
 }
