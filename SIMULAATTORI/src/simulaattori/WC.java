a11c5581b2307c6f6c9739645949e128b443a7bb
tinos <tinos@V-315-02.edu.wks.metropolia.fi>
2015-09-28 13:01:53 +0300

    yhyy

--- SIMULAATTORI/src/simulaattori/WC.java
+++ SIMULAATTORI/src/simulaattori/WC.java
@@ -17,6 +17,18 @@ public class WC {
     public WC() {
 
     }
+    
+    public String gotoPönttö() {
+        hp.lisää(10);
+        return "Olet nyt pöntöllä. Saat 10 helaa lisää. Sinulla on " + hp.getHp()
+        + ". Voit mennä kylpyyn tai takaisin käytävään.";
+    }
+    
+    public String enterAmme() {
+        hp.lisää(10);
+        return "Olet kylvyssä. Saat 10 helaa. Sinulla on " + hp.getHp() +
+                "Voit poistua wc:stä tai mennä pöntölle.";
+    }
 
     public WC(Ihminen hp) {
         this.hp = hp;
