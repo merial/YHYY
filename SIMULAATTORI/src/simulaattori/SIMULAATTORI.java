a87828dd1221dfb4e116d4fe7fce61818ceee249
merial <meri.alho@gmail.com>
2015-09-28 14:14:27 +0300

    lisää shittii

--- SIMULAATTORI/src/simulaattori/SIMULAATTORI.java
+++ SIMULAATTORI/src/simulaattori/SIMULAATTORI.java
@@ -11,7 +11,16 @@ public class SIMULAATTORI {
         char K, k, E, e;
         int valinta;
         String nimi;
-        
+        Ihminen hp = new Ihminen();
+        R_N_G numero = new R_N_G();
+        Syöpä tauti = new Syöpä(numero);
+        Jääkaappi ruoka = new Jääkaappi(hp,5);
+        Keittiö syo = new Keittiö(ruoka);
+        Makuuhuone uni = new Makuuhuone();
+        Hukkuminen huku = new Hukkuminen();
+        Uimaallas allas = new Uimaallas();
+        Tulipalo palo = new Tulipalo(hp);
+        WC vessa = new WC();
         
         
         JOptionPane.showMessageDialog(null, "Tervetuloa SIMulaatioon!\n"
@@ -19,9 +28,9 @@ public class SIMULAATTORI {
         
         
         nimi = JOptionPane.showInputDialog(null, "Kerro nimesi");
+        hp.setNimi(nimi);
         
-        
-        JOptionPane.showMessageDialog(null, "Nimesi on siis "+omaKontrolleri.getNimi());
+        JOptionPane.showMessageDialog(null, "Hei "+hp.getNimi()+"!");
         
         System.exit(0);
         
