f5a60060ae13003cc51e3056646a8fd05d8f1783
tinos <tinos@V-315-02.edu.wks.metropolia.fi>
2015-10-02 08:38:09 +0300

    Merge origin/master
    Conflicts:
    	SIMULAATTORI/src/simulaattori/Hukkuminen.java
    	SIMULAATTORI/src/simulaattori/WC.java

--- SIMULAATTORI/src/simulaattori/SIMULAATTORI.java
+++ SIMULAATTORI/src/simulaattori/SIMULAATTORI.java
@@ -10,14 +10,26 @@ public class SIMULAATTORI {
         char K, k, E, e;
         int valinta;
         String nimi;
-
+        Ihminen hp = new Ihminen();
+        R_N_G numero = new R_N_G();
+        Syöpä tauti = new Syöpä(numero);
+        Jääkaappi ruoka = new Jääkaappi(hp,5);
+        Keittiö syo = new Keittiö(ruoka);
+        Makuuhuone uni = new Makuuhuone();
+        Hukkuminen huku = new Hukkuminen();
+        Uimaallas allas = new Uimaallas();
+        Tulipalo palo = new Tulipalo(hp);
+        WC vessa = new WC();
+        
+        
         JOptionPane.showMessageDialog(null, "Tervetuloa SIMulaatioon!\n"
                 + "Voit liikkua huoneissa ja tehdä jotain ja jotain öö");
 
         nimi = JOptionPane.showInputDialog(null, "Kerro nimesi");
-
-        JOptionPane.showMessageDialog(null, "Nimesi on siis " + omaKontrolleri.getNimi());
-
+        hp.setNimi(nimi);
+        
+        JOptionPane.showMessageDialog(null, "Hei "+hp.getNimi()+"!");
+        
         System.exit(0);
 
     }
